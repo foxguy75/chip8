ae3df7370989b13db88dea8c3f047f11993f6c28
Ben <camburjb@mail.uc.edu>
2020-06-08 17:35:25 -0400

    Initial Commit

---
+++ chip8/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  chip8
+//
+//  Created by Ben Camburn on 6/8/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
